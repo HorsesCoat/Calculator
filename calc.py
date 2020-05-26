549374105eb06fd44ce47249c236d56d507ed8ba
Eugeny Kim <karmakagyu108@gmail.com>
2020-05-26 23:44:30 +0300

    Calculator v. 0.4

--- calc.py
+++ calc.py
@@ -1,50 +1,50 @@
 '''
-Calculator v. 0.2
+Calcultor v. 0.3
 '''
 
 def input_number():
-	num = input("Введите число:")
-	num = float(num)
-	return num
+    num = input("Введите число: ")
+    num = float(num)
+    return num
 
 def input_oper():
-	oper = input("Операция('*','','+','-'):")
-	return oper
+    oper = input("Операция('*','/', '+', '-'): ")
+    return oper
 
 def calc_me(x,y, oper):
-	if oper == '*':
-		return x * y
-	elif oper == '/':
-	   # если делитель == 0 то возвращаем ошибку
-	   if y == 0:
-	      return "ERROR: Divide by zero!"
-	    else:
-		return x / y
-	elif oper == '+':
-		return x + y
-	elif oper == '-':
-		return x-y
-	else:
-	  return "ERROR: Uknow operation"
+    if oper == '*':
+        return x * y
+    elif oper == '/':
+        # если делитель == 0 то возвращаем ошибку
+        if y == 0:
+            return "ERROR: Divide by zero!"
+        else:
+            return x / y
+    elif oper == '+':
+        return x + y
+    elif oper == '-':
+        return x - y
+    else:
+        return "ERROR: Uknow operation"
 
 def body():
-  # результат работы функции input_number запишется в переменную number1
-  number1 = input_number()
-  # результат работы функции input_oper запишется в переменную oper
-  oper = input_oper()
-  # результат работы функции input_number запишется в переменную number2
-  number2 = input_number()
-  # вызываем функцию calc_me с переменными которые мы ранее получили
-  # результат запишем в переменную result
-  result = calc_me(number1,number2,oper)
-  # выводим результат для пользователя
-print(result)
+    # результат работы функции input_number запишется в переменную number1
+    number1 = input_number()
+    # результат работы функции input_oper запишется в переменную oper
+    oper = input_oper()
+    # результат работы функции input_number запишется в переменную number2
+    number2 = input_number()
+    # вызываем функцию calc_me с переменными которые мы ранее получили 
+    # результат запишем в переменную result
+    result = calc_me(number1,number2, oper)
+    # выводим результат для пользователя
+    print(result)
 
-# это специальное служебное условие Python
-if__name__=='__main__':
-# оно говорит, что если мы вызвали этот файл в консоли
-# то надо выполнить функцию body
-body()
+# это специальное служебное условие Питон
+if __name__ == '__main__':
+    # оно говорит, что если мы вызвали этот файл в консоли 
+    # то надо выполнить функцию body
+    body()
 
 # результат работы функции input_number запишется в переменную number1
 number1 = input_number()
